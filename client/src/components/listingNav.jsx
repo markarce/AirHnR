3cbb2a4d094c749cce26a8647ffa945890161da5
Cherry Xu <cherryxuqianyun@gmail.com>
2018-01-09 15:08:25 -0800

    rendered: photo, title, and navbar

---
+++ client/src/components/listingNav.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import Button from 'material-ui/Button';
+
+const styles = theme => ({
+    button: {
+      margin: theme.spacing.unit,
+    },
+    input: {
+      display: 'none',
+    },
+});
+  
+function ListingNav(props) {
+    const { classes } = props;
+    return (
+      <div className='listing-nav'>
+        <Button className={classes.button}>Overview</Button>
+        <Button className={classes.button}>Reviews</Button>
+        <Button className={classes.button}>The Host</Button>
+        <Button className={classes.button}>Location</Button>
+      </div>
+    );
+}
+  
+ListingNav.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+  
+export default withStyles(styles)(ListingNav);
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,12 +3,46 @@ import { render } from 'react-dom';
 import css from './styles/styles.css';
 // import 'typeface-roboto';
 import { BrowserRouter } from 'react-router-dom';
+import ListingDetails from './components/listingDetails.jsx'
+
 
 import SearchResults from './components/SearchResults'
 import Search from './components/Search.jsx'
 import NavBar from './components/NavBar.jsx'
 import $ from 'jquery'
 
+var sampleData = {
+  beds: 1,
+  address: 'Jalan Pantai Kuta, Banjar Pande Mas, Kuta, Kabupaten Badung, Bali 80361, Indonesia',
+  id: Number,
+  name: 'Beautiful SOMA Condo/Moscone/Downtown',
+  price: 300,
+  reviews_count: 32,
+  room_type: 'Entire loft',
+  star_rating: 5,
+  image_url: 'https://a0.muscache.com/im/pictures/7ce1579c-4c8b-424c-99cd-23cab046adce.jpg?aki_policy=xx_large',
+
+  bathrooms: 2,
+  maxGuests: 2,
+  tagline: 'Enjoy your relaxation time from SF life in this newly renovated Dream House decorated with modern artwork and new furniture throughout, walk to EVERYTHING in Downtown / SOMA San Francisco!',
+  description: `Enjoy your relaxation time from SF life in this newly renovated Dream House decorated with modern artwork and new furniture throughout, walk to EVERYTHING in Downtown / SOMA San Francisco! The space A bright and beautiful designed home! The the living are is large with tall ceilings which brings in the sunshine accentuate the colors in the space. Towels and Linens are fresh and available for you. Cozy up on a beautiful bed and mattress. Easily work from home with a huge desk in living area ( High-Speed Wi-Fi Available)!`,
+  amenities: {
+    wifi: true,
+    kitchen: true,
+    hotTub: false
+  },
+  houseRules: {
+    pets: false,
+    smoking: false,
+    checkIn: '3pm',
+    checkOut: '12pm',
+  },
+  cancellation: true,
+  host: {
+    id: Number,
+    name: 'Lucas'
+  }
+}
 
 class App extends React.Component {
   constructor(props) {
@@ -21,6 +55,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
+
       <NavBar/>
       <div>
         <Search onClick={this.handleClickFunction}/>
@@ -29,6 +64,9 @@ class App extends React.Component {
       <div>
         <SearchResults />
       </div>
+
+        <ListingDetails listing={sampleData}/>
+
       </div>
     );
   }
@@ -36,3 +74,5 @@ class App extends React.Component {
 }
 
 render(<App />, document.getElementById('app'));
+
+
