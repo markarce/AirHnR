02c6e19f477293cc2b1fce850153456c78879aa8
Cody Schindler <admin@codyschindler.com>
2018-01-10 14:34:48 -0800

    created a bunch of helper functions

--- database/data/index.js
+++ database/data/index.js
@@ -3,15 +3,55 @@ module.exports = {
   locations: require('./locations.json'),
 }
 let userdata = require('./users.json')
-let locationdata = require('./location.json')
+let locationdata = require('./locations.json')
 let nyll = require('./ny_lat_long.json')
 let sfll = require('./sf_lat_long.json')
 let home_types = ['Home', 'Aprtment', 'Abode', 'Townhouse', 'Mansion', 'Castle', 'Dump', 'Slum', 'House', 'Flat', 'Palace', 'Estate', 'Dwelling', 'Box', 'Co-Op', 'Lean-To', 'Cardboard Box', 'Shanty', 'Cairn', 'Alleyway', 'Flophouse', 'Couch', 'Basement'];
 
+let roomt_types = ['Couch', 'Room'];
+let house_pics = require('./pics.json');
+let pickOne = (array) => {
+  return array[Math.floor(Math.random() * array.length)]
+};
+let numberInRange = (start, stop) => {
+  //return a number in range (inclusive) or between 0- start if stop not given
+  if (stop) {
+    return Math.floor(Math.random() * (stop - start)) + start;
+  }
+  else {
+    return Math.floor(Math.random() * (start + 1));
+  }
+};
+
+let generateStay = () => {
+  let nights = numberInRange(1, 7);
+  let startDay = numberInRange(1, 21);
+  let endDay = startDay + nights;
+  let month = numberInRange(1, 12);
+  return {
+    start: new Date(`${month}/${startDay}/2018`), 
+    end: new Date(`${month}/${endDay}/2018`)
+  }
+}
+
+let generateAmenity = () => {
+  return {
+    wifi: pickOne([true, true, true, true, true, true, false]),
+    kitchen: pickOne([false, false, false, false, false, true, false]),
+    hotTub: pickOne([false, false, false, false, false, true, false])
+  };
+};
+
+generateHouseRules = () => {
+  pets: pickOne([true, false, false]),
+  smoking: pickOne([false, false, false, false, false, true, false]),
+  checkIn: '12:00 PM',
+  checkOut: '12:00 PM'
+}
 
 const createUsers = () => {
   let address_info = [['NewYork','NY', '10001'], ['San Francisco', 'CA', '94117']]
-  userdata.forEach( (dp, idx) => { 
+  return userdata.map( (dp, idx) => { 
     let city = idx > 99 ? 1 : 0;
     return {
       username: dp.username,
@@ -35,25 +75,25 @@ const createUsers = () => {
 
 const createLocations = () => {
   let address_info = [['NewYork', 'NY', '10001'], ['San Francisco', 'CA', '94117']]
-  locationdata.forEach((dp, idx) => {
-    let city = idx > 99 ? 1 : 0;
-    let cityidx = idx > 99 ? idx - 100 : idx;
+  return locationdata.map((dp, idx) => {
+    let city = idx < locationdata.length ? 0 : 1;
+    let cityidx = idx < locationdata.length ? idx : idx - 100;
     let cityll = city === 1 ? sfll : nyll;
     return {
-      name: `${dp.adjective.toUpperCase()} ${address_info[city][0]} ${home_types[Math.floor(Math.random()* home_types.length])}`,
+      name: `${dp.adjective.toUpperCase()} ${address_info[city][0]} ${pickOne(home_types)}`,
       tagline: dp.tagline,
       description: dp.description,
-      image_url: dp.image_url,
-      room_type: dp.room_type,
-      max_guests: dp.max_guests,
-      beds: dp.beds,
-      bathrooms: dp.bathrooms,
+      image_url: pickOne(house_pics),
+      room_type: pickOne(room_types),
+      max_guests: numberInRange(1, 5),
+      beds: numberInRange(1, 6),
+      bathrooms: numberInRange(1, 3),
       latitude: cityll[cityidx].latitude,
       longitude: cityll[cityidx].longitude,
-      cancellation: dp.cancellation,
-      amenities: dp.amenities,
-      house_rules: dp.house_rules,
-      host_id: idx,
+      cancellation: pickOne([true, false]),
+      amenities: generateAmenity(),
+      house_rules: generateHouseRules(),
+      host_id: idx % userdata.length,
       address_street: dp.address_street,
       address_city: address_info[city][0],
       address_region: address_info[city][1],
@@ -61,24 +101,3 @@ const createLocations = () => {
     }
   })
 }
-
-
-table.string('name');
-table.string('tagline', 1000);
-table.string('description', 3000);
-table.string('address_street');
-table.string('address_city');
-table.string('address_region');
-table.string('address_postal_code');
-table.float('latitude');
-table.float('longitude');
-table.string('image_url', 1000);
-table.string('room_type');
-table.integer('max_guests');
-table.integer('beds');
-table.integer('bathrooms');
-table.boolean('cancellation');
-table.json('amenities');
-table.json('house_rules');
-table.integer('host_id');
-table.foreign('host_id').references('users.id');
