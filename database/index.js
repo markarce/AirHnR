390859b9970db66285b6ec308aa2618a02e3feaa
Cody Schindler <admin@codyschindler.com>
2018-01-10 00:24:01 -0800

    finished db query for locations around a point

--- database/index.js
+++ database/index.js
@@ -3,41 +3,17 @@ const knex = require('knex')({
   client: 'pg',
   connection: config.database_url + '/airhnr'
 });
+const knexPostgis = require('knex-postgis');
 const bookshelf = require('bookshelf')(knex);
 
-knex.schema.createTable('users', table => {
-  table.increments('id').primary();
-  table.string('username');
-  table.string('firstName');
-  table.string('lastName');
-  table.timestamps(true, true);
-  // table.foreign('paper_id')
-  //   .references('papers.id');
-}).then();
+const getLocationsNear = (lat, long, radius) => {
+  //lat, long of center of map, and distance radius in meters
+  //returns a promise of data
+  query = `SELECT * FROM locations WHERE ST_Distance(ST_SetSRID(ST_MakePoint(${long}, ${lat})::GEOGRAPHY, 4326), ST_SetSRID(ST_MakePoint(longitude, latitude), 4326)::GEOGRAPHY) <= ${radius}`;
+  return bookshelf.knex.raw(query)
+  // bookshelf.knex.raw(query).then(res => console.log(res)).catch(err => console.log(err));
+};
 
-// console.log(knex)
-
-
-// var User = bookshelf.Model.extend({
-//   tableName: 'users',
-//   posts: function () {
-//     return this.hasMany(Posts);
-//   }
-// });
-
-// var Posts = bookshelf.Model.extend({
-//   tableName: 'messages',
-//   tags: function () {
-//     return this.belongsToMany(Tag);
-//   }
-// });
-
-// var Tag = bookshelf.Model.extend({
-//   tableName: 'tags'
-// })
-
-// User.where('id', 1).fetch({ withRelated: ['posts.tags'] }).then(function (user) {
-//   console.log(user.related('posts').toJSON());
-// }).catch(function (err) {
-//   console.error(err);
-// });
+module.exports = {
+  getLocationsNear: getLocationsNear
+};
---
+++ docs/sprint_1_retro.md
@@ -0,0 +1,18 @@
+What have we done well this sprint? 
+- Cooperation has been good.
+- Being on task and planning what we want to do before coding.
+- Balancing working together and apart. 
+- Let's continue to ramp up from here.
+- Planing and execution went well.
+- Organization was useful.
+- Everyone was contributing and no huge blockers yet.
+
+What do we need to improve upon?
+- Being a conjurer as opposed to a scribe with new technology.
+- Timeboxing / focusing on core features.
+- Tasks to work on - defining tasks on an ongoing basis.
+- Give someone the task of planning for the next sprint at the expense of a couple user story points.
+
+What are we doing and who is the assignee?
+- Finish remaining tasks - everyone.
+- Cody will work on fleshing out the database.
