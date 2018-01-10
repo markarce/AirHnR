f7c69048abae1994ba2ce455b0fb9778b11f1307
Santiago Iraola <iraola.santi@gmail.com>
2018-01-09 16:58:20 -0800

    using process variable config vars

--- config/index.js
+++ config/index.js
@@ -2,5 +2,5 @@ require('dotenv').config();
 
 module.exports = {
   serverPort: process.env.PORT,
-  googleKey: 'AIzaSyA8RPm6ALHvSqHaH7bj7x5Ad_01-MSUI_o'
+  googleKey: process.env.GOOGLE_GEOCODING_KEY
 };
