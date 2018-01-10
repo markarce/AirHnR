e31c7a2f1d2cba43ae01fd3b23eab93d6f34f2c9
Jhovahn <nhavohj@gmail.com>
2018-01-09 19:27:13 -0800

    created navbar, able to send input data to server

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,22 +3,36 @@ import { render } from 'react-dom';
 import css from './styles/styles.css';
 // import 'typeface-roboto';
 import { BrowserRouter } from 'react-router-dom';
-import SearchResults from './components/SearchResults'
 
+import SearchResults from './components/SearchResults'
+import Search from './components/Search.jsx'
+import NavBar from './components/NavBar.jsx'
+import $ from 'jquery'
 
 
 class App extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {};
   }
 
+
+
   render() {
     return (
       <div>
+
         <div>App is loaded </div>
       <SearchResults />
+      <div>
+        <NavBar/>
+      </div>
+      <br/>
+      <div>
+        <Search onClick={this.handleClickFunction}/>
+      </div>
+      <div>App is loaded</div>
+
       </div>
     );
   }
--- server/index.js
+++ server/index.js
@@ -1,9 +1,25 @@
 const config = require('../config');
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(express.json());
+
+
+app.post('/search', function(req, res){
+  res.send(req.body)
+})
+
+app.get('/api/listings',function(req, res){
+    console.log(req.query)
+    res.json("Hello")
+
+})
+
 
 app.listen(config.serverPort, () => {
   console.log(`Server listening on port ${config.serverPort}`)
