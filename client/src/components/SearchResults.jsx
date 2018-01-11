2bd88cae179135d085a6d74341d97c3c4503fae0
Santiago Iraola <iraola.santi@gmail.com>
2018-01-10 23:41:30 -0800

    Rendering map component on search results page

--- client/src/components/SearchResults.jsx
+++ client/src/components/SearchResults.jsx
@@ -6,6 +6,8 @@ import { BrowserRouter } from 'react-router-dom';
 import data from '../../../lib/dummyData.js';
 import SearchResult from './SearchResult.jsx'
 import Grid from 'material-ui/Grid';
+import MapContainer from './MapContainer';
+import $ from 'jquery';
 
 class SearchResults extends React.Component {
   constructor(props) {
@@ -13,7 +15,13 @@ class SearchResults extends React.Component {
     this.state = {
       fakeResults: data.testSearchResults
     }
+
+    this.node = '';
     this.handleListingClick = this.handleListingClick.bind(this);
+  }
+
+  componentDidMount() {
+    this._setMapPosition();
   }
 
   handleListingClick(listingID) {
@@ -22,25 +30,42 @@ class SearchResults extends React.Component {
 
   render() {
     console.log('state', this.state);
-
     return (
-      <Grid container={true} spacing={8}>
-        { this.state.fakeResults.map(listing => (
-          <SearchResult 
-            item={true}
-            key={listing.id}
-            id={listing.id}
-            roomtype={listing.room_type}
-            beds={listing.beds}
-            name={listing.name}
-            price={listing.price}
-            rating={listing.star_rating}
-            image={listing.image_url}
-            handleClick={this.handleListingClick}
-          />
-        ))}
-      </Grid>
+      <div className="listing-details">
+      <div className="listings">
+        <Grid container={true} spacing={8}>
+          { this.state.fakeResults.map(listing => (
+            <SearchResult 
+              item={true}
+              key={listing.id}
+              id={listing.id}
+              roomtype={listing.room_type}
+              beds={listing.beds}
+              name={listing.name}
+              price={listing.price}
+              rating={listing.star_rating}
+              image={listing.image_url}
+              handleClick={this.handleListingClick}
+            />
+          ))}
+        </Grid>
+      </div>
+      <div className="map-container" ref={node => this.node = node}>
+        <MapContainer listings={data.testSearchResults} searchedLocation={{lat: 37.89, lon: -122.432758}}/>
+      </div>
+      </div>
     );
+  }
+
+  _setMapPosition() {
+    let divNodes = $(this.node).find('div');
+    $(divNodes[0]).css({
+      width: '100%',
+      height: '100%'
+    });
+    $(divNodes[1]).css({
+      position: 'relative'
+    });
   }
 }
 
