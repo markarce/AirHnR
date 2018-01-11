1d6cf7121e0ad7fe8985e442e93102ee7c9f8463
Cherry Xu <cherryxuqianyun@gmail.com>
2018-01-11 11:07:27 -0800

    updated booking select css

--- client/src/components/BookingGuest.jsx
+++ client/src/components/BookingGuest.jsx
@@ -10,7 +10,6 @@ const styles = theme => ({
   container: {
     display: 'flex',
     flexWrap: 'wrap',
-<<<<<<< HEAD
     autoWidth: false,
   },
   formControl: {
@@ -22,15 +21,6 @@ const styles = theme => ({
     // marginTop: theme.spacing.unit * 2,
     // width: 300,
     // autoWidth: false
-=======
-  },
-  formControl: {
-    margin: theme.spacing.unit,
-    minWidth: 200,
-  },
-  selectEmpty: {
-    marginTop: theme.spacing.unit * 2,
->>>>>>> completed booking feature
   },
 });
 
@@ -60,7 +50,6 @@ class BookingGuest extends React.Component {
   };
 
   render() {
-<<<<<<< HEAD
     const { classes } = this.props;
 
     return (
@@ -68,14 +57,6 @@ class BookingGuest extends React.Component {
         <FormControl className={classes.formControl}>
           <InputLabel id='guest-lable' htmlFor="guests-simple">Guests</InputLabel>
           <Select id='guest-select'
-=======
-
-    return (
-      <form className={this.props.container} autoComplete="off">
-        <FormControl className={this.props.formControl}>
-          <InputLabel htmlFor="guests-simple">Guests</InputLabel>
-          <Select
->>>>>>> completed booking feature
             value={this.state.guests}
             onChange={this.handleChange.bind(this)}
             input={<Input name="guests" id="guests-simple" />}
