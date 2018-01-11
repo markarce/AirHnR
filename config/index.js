b4f52bcf759fef20f842546bbff08b72b52bb48f
Cody Schindler <admin@codyschindler.com>
2018-01-11 10:26:40 -0800

    modifying things to get the db working

--- config/index.js
+++ config/index.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 
-
-
 module.exports = {
   serverPort: process.env.PORT,
   googleKey: process.env.GOOGLE_GEOCODING_KEY,
-  database_url: process.env.DATABASE_URL
+  database_url: process.env.DATABASE_URL,
+  heroku_db_host: process.env.HEROKU_DB_HOST,
+  heroku_db_user: process.env.HEROKU_DB_USER,
+  heroku_db_password: process.env.HEROKU_DB_PASS,
+  heroku_db_db: process.env.HEROKU_PG_DB
 };
 
 // console.log(module.exports);
