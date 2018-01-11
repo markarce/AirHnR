71b5ad6009a69589d8fd1b5786e432107c6f6d12
Cherry Xu <cherryxuqianyun@gmail.com>
2018-01-11 01:18:40 -0800

    completed booking feature

---
+++ client/src/components/BookingGuest.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import Input, { InputLabel } from 'material-ui/Input';
+import { MenuItem } from 'material-ui/Menu';
+import { FormControl, FormHelperText } from 'material-ui/Form';
+import Select from 'material-ui/Select';
+
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  formControl: {
+    margin: theme.spacing.unit,
+    minWidth: 200,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing.unit * 2,
+  },
+});
+
+class BookingGuest extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      guests: 0
+    };
+  }
+
+  renderSelect () {
+    let guests = [];
+    for (let i = 1; i <= this.props.maxGuests; i++) {
+      guests.push(i);
+    }
+
+    return guests.map(guest => {
+      return <MenuItem key={guest} value={guest}>{guest}</MenuItem>
+    });
+  }  
+
+  handleChange (event) {
+    this.setState({
+      guests: event.target.value
+    });
+  };
+
+  render() {
+
+    return (
+      <form className={this.props.container} autoComplete="off">
+        <FormControl className={this.props.formControl}>
+          <InputLabel htmlFor="guests-simple">Guests</InputLabel>
+          <Select
+            value={this.state.guests}
+            onChange={this.handleChange.bind(this)}
+            input={<Input name="guests" id="guests-simple" />}
+          >
+            {this.renderSelect()}
+          </Select>
+        </FormControl>
+      </form>
+    );
+  }
+}
+
+BookingGuest.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(BookingGuest);
