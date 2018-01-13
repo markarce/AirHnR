338fe59a0fba2e4f206c3be02a64af089e8d969b
Santiago Iraola <iraola.santi@gmail.com>
2018-01-13 13:16:39 -0800

    user is logged in automatically once they create their account.

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -67,20 +67,6 @@ class App extends React.Component {
     return `${year.toString()}-${month.toString()}-${day.toString()}`
   }
 
-  getSimpleDate (dateObj) {
-    const date = new Date(dateObj);
-    const year = date.getFullYear();
-    var month = date.getMonth() + 1;
-    var day = date.getDate();
-    if (month < 10) {
-      month = '0' + month;
-    }
-    if (day < 10) {
-      day = '0' + date;
-    }
-    return `${year.toString()}-${month.toString()}-${day.toString()}`
-  }
-
   searchTerm(term) {
     this.setState({ query: term });
   }
