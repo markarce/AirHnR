fc40a1f507371377e15063ee5256394ff7fd99e7
Santiago Iraola <iraola.santi@gmail.com>
2018-01-14 19:04:24 -0800

    if no results are returned from the search a 'no results' message is rendered

--- client/src/components/SearchResults.jsx
+++ client/src/components/SearchResults.jsx
@@ -8,6 +8,7 @@ import Grid from 'material-ui/Grid';
 import { withStyles } from 'material-ui/styles';
 import MapContainer from './MapContainer';
 import $ from 'jquery';
+import FeaturedPlaces from './FeaturedPlaces';
 
 class SearchResults extends React.Component {
   constructor(props) {
@@ -28,36 +29,48 @@ class SearchResults extends React.Component {
 
   render() {
     var listings = this.props.results;
-    return (
-      <div className="listing-details">
-        <div className="listings">
-          <Grid container spacing={16}>
-            { listings.map(listing => (
-              <Grid item key={listing.id}>
-                <SearchResult 
-                  key={listing.id}
-                  id={listing.id}
-                  roomtype={listing.room_type}
-                  beds={listing.beds}
-                  name={listing.name}
-                  price={listing.price}
-                  rating={listing.average_stars}
-                  image={listing.image_url}
-                  handleClick={this.handleListingClick}
-                />
-              </Grid>
-            ))}
-          </Grid>
+    if(listings.length) {
+      return (
+        <div className="listing-details">
+          <div className="listings">
+            <Grid container spacing={16}>
+              { listings.map(listing => (
+                <Grid item key={listing.id}>
+                  <SearchResult 
+                    key={listing.id}
+                    id={listing.id}
+                    roomtype={listing.room_type}
+                    beds={listing.beds}
+                    name={listing.name}
+                    price={listing.price}
+                    rating={listing.average_stars}
+                    image={listing.image_url}
+                    handleClick={this.handleListingClick}
+                  />
+                </Grid>
+              ))}
+            </Grid>
+          </div>
+          <div className="map-container" ref={node => this.node = node}>
+            <MapContainer 
+              listings={listings} 
+              handleMapDrag={this.props.handleMapDrag} 
+              searchedLocation={this.props.mapCenter}
+            />
+          </div>
         </div>
-        <div className="map-container" ref={node => this.node = node}>
-          <MapContainer 
-            listings={listings} 
-            handleMapDrag={this.props.handleMapDrag} 
-            searchedLocation={this.props.mapCenter}
-          />
+      );
+    } else {
+      return (
+        <div style={{display: 'flex', margin: '0 5%'}}>
+          <img src="https://www.bhiner.com/img/noResult.png" alt="No Results"/>
+          <div style={{marginLeft: '10%'}}>
+            <h4>No results found</h4>
+            <p>Please search for a different location</p>
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
 
   _setMapPosition() {
